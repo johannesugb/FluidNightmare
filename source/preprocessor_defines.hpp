78329237f8de258894ea56653021a1b1fca4d7ad
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2021-05-07 19:22:19 +0200

    Restructured the code from what was previously contained in one big invokee into two: -> fluid_nightmare_main -> triangle_mesh_geometry_manager

---
+++ source/preprocessor_defines.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+// Set this compiler switch to 1 to enable hot reloading of
+// the ray tracing pipeline's shaders. Set to 0 to disable it.
+#define ENABLE_SHADER_HOT_RELOADING_FOR_RAY_TRACING_PIPELINE 1
+
+// Set this compiler switch to 1 to make the window resizable
+// and have the pipeline adapt to it. Set to 0 ti disable it.
+#define ENABLE_RESIZABLE_WINDOW 1
